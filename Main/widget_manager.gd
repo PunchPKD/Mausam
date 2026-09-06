414afa3a532431ea3de74f74f18e4e9eee47ca82
Punch PKD <120797546+PunchPKD@users.noreply.github.com>
2026-09-06 16:25:48 +0530

    backup

--- Main/widget_manager.gd
+++ Main/widget_manager.gd
@@ -19,14 +19,15 @@ func on_add_widget_list(widgetData: BaseWidgetData) -> void:
 	for widget in currentWidgets.widgets:
 		SignalBus.RemoveWidget.emit(widget)
 	for widget in widgetData.widgets:
-		currentWidgets.widgets.append(widget)
 		SignalBus.AddWidget.emit(widget)
 
 func on_add_widget(widgetScene: PackedScene) -> void:
 	get_widget(widgetScene).visible = true
+	currentWidgets.widgets.append(widgetScene)
 
 func on_remove_widget(widgetScene: PackedScene) -> void:
 	get_widget(widgetScene).visible = false
+	currentWidgets.widgets.erase(widgetScene)
 
 func get_widget(widgetScene: PackedScene) -> Widget:
 	if widgets.has(widgetScene.resource_path):
@@ -35,4 +36,13 @@ func get_widget(widgetScene: PackedScene) -> Widget:
 	widgets[widgetScene.resource_path] = tempWidget
 	widgetContainer.add_child(tempWidget)
 	return tempWidget
-	
+
+func reassign_widget_rank(widgetScene: PackedScene) -> void:
+	var totalActiveWidget: int = currentWidgets.widgets.size()
+	var tempWidget : Widget = get_widget(widgetScene)
+	if tempWidget.get_index()+1 <= totalActiveWidget/3:
+		tempWidget.widgetRank = tempWidget.WidgetRanks.TOP
+	elif tempWidget.get_index()+1 <= (totalActiveWidget/3)*2:
+		tempWidget.widgetRank = tempWidget.WidgetRanks.MIDDLE
+	else :
+		tempWidget.widgetRank = tempWidget.WidgetRanks.BOTTOM
--- Scripts/widget.gd
+++ Scripts/widget.gd
@@ -1,12 +1,45 @@
 extends Control
 class_name Widget
 
+enum InformationModes{SIMPLE, DETAILED}
+enum WidgetRanks{TOP, MIDDLE, BOTTOM}
+
+@export var simpleInformationElement: Control
+@export var detailedInformationElement: Control
+@export var focusedInformationElement: Control
+
+var informationMode: InformationModes = InformationModes.SIMPLE
+var widgetRank: WidgetRanks = WidgetRanks.BOTTOM:
+	set(value):
+		if value == WidgetRanks.TOP:
+			change_information_element(focusedInformationElement)
+		elif value == WidgetRanks.MIDDLE:
+			change_information_element(detailedInformationElement)
+		elif value == WidgetRanks.BOTTOM:
+			change_information_element(simpleInformationElement)
+var currentInformationElement: Control
+
 func _ready() -> void:
-	pass
+	if simpleInformationElement:
+		simpleInformationElement.visible = false
+	if detailedInformationElement:
+		detailedInformationElement.visible = false
+	currentInformationElement = simpleInformationElement
+	if informationMode == InformationModes.SIMPLE:
+		change_information_element(simpleInformationElement)
+	else :
+		change_information_element(detailedInformationElement)
 	
 func _gui_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Click"):
 		pass
+
+func change_information_element(element: Control) -> void:
+	if element:
+		self.custom_minimum_size = element.size
+		currentInformationElement.visible = false
+		currentInformationElement = element
+		currentInformationElement.visible = true
 
 func remove() -> void:
 	SignalBus.RemoveWidget.emit(self)
