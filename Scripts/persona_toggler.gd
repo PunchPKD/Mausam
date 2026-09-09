0fa1c8a1703d030743e0d690f67342e4311946af
Punch PKD <120797546+PunchPKD@users.noreply.github.com>
2026-09-10 02:07:41 +0530

    Extension Day 2

--- Scripts/persona_toggler.gd
+++ Scripts/persona_toggler.gd
@@ -5,6 +5,12 @@ class_name PersonaToggler
 @export var panel: Panel
 @export var label: Label
 
+var time: float
+
 func _gui_input(event: InputEvent) -> void:
-	if Input.is_action_just_released("Click"):
+	if Input.is_action_just_pressed("Click"):
+		time = 0
+	if Input.is_action_pressed("Click"):
+		time += 0.1
+	if Input.is_action_just_released("Click") and time < 6:
 		SignalBus.AddWidgetList.emit(widgetData)
