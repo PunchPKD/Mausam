d6fcf69d50377dbc2e4929a49fa0caf7cf50df9f
Punch PKD <120797546+PunchPKD@users.noreply.github.com>
2026-09-13 16:11:37 +0530

    Prototype Showcase

--- Scripts/location_selector.gd
+++ Scripts/location_selector.gd
@@ -2,8 +2,20 @@ extends Control
 class_name LocationSelector
 
 @export var saveFile: BaseSaveFile
+@export var tempLabel: Label
+@export var weatherLabel: Label
+
+func _ready() -> void:
+	SignalBus.UpdateData.connect(on_update_data)
+	tempLabel.text = saveFile.widgetsData.get(DataTypeEnum.DataTypes.Temp)
+	weatherLabel.text = WeatherTypeEnum.WeatherType.find_key(int(saveFile.widgetsData.get(DataTypeEnum.DataTypes.Weather)))
 
 func _gui_input(event: InputEvent) -> void:
 	if Input.is_action_just_released("Click"):
 		SaveManager.load_save_file(saveFile)
 		SignalBus.ToggleLocationSelectionOverlay.emit()
+
+func on_update_data(data:Dictionary[DataTypeEnum.DataTypes, String]) -> void:
+	if SaveManager.currentSaveFile == saveFile:
+		tempLabel.text = data.get(DataTypeEnum.DataTypes.Temp)
+		weatherLabel.text = WeatherTypeEnum.WeatherType.find_key(int(data.get(DataTypeEnum.DataTypes.Weather)))
--- Scripts/profile_selection_element.gd
+++ Scripts/profile_selection_element.gd
@@ -4,6 +4,14 @@ class_name ProfileSelectionElement
 @export var tags: Array[TagsEnum.WeatherTags]
 @export var selected: bool = false
 
+var time: float
+
+func _gui_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Click"):
+		time = 0
+	if Input.is_action_pressed("Click"):
+		time += 0.1
+
 func _ready() -> void:
 	SignalBus.UpdateWidgetScore.connect(on_update_data)
 
@@ -13,4 +21,5 @@ func on_update_data() -> void:
 			SignalBus.IncreaseWidgetScore.emit(tag)
 
 func _on_toggled(toggled_on: bool) -> void:
-	selected = !selected
+	if time < 2:
+		selected = !selected
